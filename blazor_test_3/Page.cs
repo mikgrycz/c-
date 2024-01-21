ca30decd040a3104bf9d27d15d6f4949a124c8ce
mikgrycz <81023852+mikgrycz@users.noreply.github.com>
2024-01-21 18:06:12 +0100

    new files

---
+++ blazor_test_3/Page.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace blazor01 {
+	/// <summary>
+	/// Clas that representing single page
+	/// </summary>
+	public class Page {
+		public string Url { private get; set; }
+		public string Name { private get; set; }
+		public string Html {
+			get {
+				return "... '" + this.Url + "'..." + this.Name + "…"; //to ma zwracać link do strony w kontenerze
+			}
+			private set { }
+		}
+	}
+}
